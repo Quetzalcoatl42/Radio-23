7370f556008dc3737375a1de7117dfee221a915e
Quetzal <quetzalcoatl@berkeley.edu>
2023-01-17 14:42:09 -0800

    I did it

---
+++ plot.py
@@ -0,0 +1,7 @@
+import ugradio
+import matplotlib.pyplot as plt
+import numpy as np
+coni = ugradio.sdr.SDR(fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
+data = coni.capture_data()
+plt.plot(data[0])
+plt.show()
