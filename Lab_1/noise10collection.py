beb22c8b5e9552d15d7561e09fb4628666515794
Quetzal <quetzalcoatl@berkeley.edu>
2023-01-29 15:27:18 -0800

    commiting

--- Lab_1/noise10collection.py
+++ Lab_1/noise10collection.py
@@ -2,17 +2,9 @@ import ugradio
 import matplotlib.pyplot as plt
 import numpy as np
 coni = ugradio.sdr.SDR(fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]), sample_rate =2.2e6)
-data = coni.capture_data()
-
-
-
-
-
-
-np.save('DSB600-570.npy', data)
-
-
+samples = int(2048)
+blocks = int(50)
+data = coni.capture_data(nsamples = samples, nblocks = blocks)
+np.save('noise2048block50.npy', data)
 plt.plot(data[0])
 plt.show()
-#import IPython
-#IPython.embed()
